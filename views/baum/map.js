4c24ed0baa370f6391eef29d9aedb3cc1232bca1
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-07-10 00:01:29 +0200

    view für baum entworfen

---
+++ views/baum/map.js
@@ -0,0 +1,11 @@
+﻿function(doc) {
+	if (doc.Gruppe && doc.Gruppe === "Fauna") {
+		emit ([doc.Gruppe, doc.Index.Felder.Klasse, doc.Index.Felder.Ordnung, doc.Index.Felder.Familie, doc.Index.Felder.Gattung], null);
+	}
+	if (doc.Gruppe && doc.Gruppe === "Flora") {
+		emit ([doc.Gruppe, doc.Index.Felder.Familie, doc.Index.Felder.Gattung], null);
+	}
+	if (doc.Gruppe && doc.Gruppe === "Moose") {
+		emit ([doc.Gruppe, doc.Index.Felder.Klasse, doc.Index.Felder.Familie, doc.Index.Felder.Gattung], null);
+	}
+}
