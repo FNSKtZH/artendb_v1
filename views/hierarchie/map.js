386d0b97694ff48ca53e7f3028dbd56b69f5bdef
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-04-16 22:36:39 +0200

    readme aktualisiert

--- views/hierarchie/map.js
+++ views/hierarchie/map.js
@@ -2,7 +2,7 @@
 	if (doc.Typ && doc.Typ === "Objekt" && doc.Taxonomie && doc.Taxonomie.Daten && doc.Taxonomie.Daten.Hierarchie && doc.Taxonomie.Daten.Hierarchie.length > 0) {
 		for (var i=0; i<doc.Taxonomie.Daten.Hierarchie.length; i++) {
 			if (doc.Taxonomie.Daten.Hierarchie[i].GUID) {
-				emit(doc.Taxonomie.Daten.Hierarchie[i].GUID, null);
+				emit(doc.Taxonomie.Daten.Hierarchie[i].GUID, doc._id);
 			}
 		}
 	}
