e636d56b5ed126885e69d2e71e07afc38b5a6c97
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-09-08 12:49:07 +0200

    Daten können für das Artenlistentool exportiert werden

--- lists/export_alt_mit_synonymen_direkt.js
+++ lists/export_alt_mit_synonymen_direkt.js
@@ -3,103 +3,20 @@ function(head, req) {
 	start({
 		"headers": {
 			"Content-Type": "text/csv",
-			"Content-disposition": "attachment;filename=Arteigenschaften_mit_Synonymen.csv",
+			"Content-disposition": "attachment;filename=Arteigenschaften_mit_Synonymen_fuer_Artenlistentool.csv",
 			"Accept-Charset": "utf-8"
 		}
 	});
 
-	var row, Objekt, total_rows,
-		rückgabeObjekt = {},
+	var row, Objekt,
 		exportObjekte = [],
 		exportObjekt,
-		filterkriterienObjekt = {"rows": []},
-		felder = [],
-		feldwert,
 		gruppen,
-		felderObjekt,
-		schonKopiert,
-		objektKopiert,
-		objektHinzufügen,
-		DsName_z,
-		Feldname_z,
-		Filterwert_z,
-		Vergleichsoperator_z,
-		Datensammlung,
-		beziehungssammlungen, datensammlungen,
-		beziehungssammlungen_aus_synonymen, datensammlungen_aus_synonymen,
-		Beziehungssammlung,
-		Beziehung,
-		dsExistiertSchon,
-		dsExistiert;
+		beziehungssammlungen_aus_synonymen, datensammlungen_aus_synonymen;
 	var _ = require("lists/lib/underscore");
 
-	//Standardfelder aufbauen
-	felder = [
-		{
-			DsTyp: "Datensammlung",
-			DsName: "Artengruppen",
-			Feldname: "GIS-Layer",
-			FeldnameOutput: "GISLayer"
-		},
-		{
-			DsTyp: "Taxonomie",
-			DsName: "Taxonomie(n)",
-			Feldname: "Taxonomie ID",
-			FeldnameOutput: "Ref"
-		},
-		{
-			DsTyp: "Taxonomie",
-			DsName: "Taxonomie(n)",
-			Feldname: "Artname",
-			FeldnameOutput: "NameLat"
-		},
-		{
-			DsTyp: "Taxonomie",
-			DsName: "Taxonomie(n)",
-			Feldname: "Name Deutsch",
-			FeldnameOutput: "NameDeu"
-		},
-		{
-			DsTyp: "Datensammlung",
-			DsName: "ZH Artwert (1995)",
-			Feldname: "Artwert",
-			FeldnameOutput: "Artwert"
-		},
-		{
-			DsTyp: "Datensammlung",
-			DsName: "ZH Artwert (1995)",
-			Feldname: "Artwert Zusatz",
-			FeldnameOutput: "AwZusatz"
-		},
-		{
-			DsTyp: "Taxonomie",
-			DsName: "ZH Artengruppen",
-			Feldname: "Betrachtungsdistanz (m)",
-			FeldnameOutput: "Distanz"
-		}
-	]
-	
 	//gruppen ist vorgegeben
 	gruppen = ["Fauna", "Flora"];
-
-	//übergebene Variabeln extrahieren
-	for (var i in req.query) {
-
-		//für alt keine Filter nötig, daher alles damit zusammenhängende gelöscht
-
-		//Felder: Damit später allenfalls weitere Felder dynamisch gewählt werden können, diesen Code belassen
-		if (i === "felder") {
-			felderObjekt = JSON.parse(req.query[i]);
-			//mitgelieferte Felder anhängen
-			for (var x=0; felderObjekt.rows.length; x++) {
-				felder.push(felderObjekt.rows[x]);
-			}
-		}
-
-		//nur_ds ist immer false und wird daher nicht benötigt
-
-		//bez sind nie in Zeilen, daher entfernt
-	}
 
 	//arrays für sammlungen aus synonymen gründen
 	beziehungssammlungen_aus_synonymen = [];
@@ -109,61 +26,55 @@ function(head, req) {
 	while (row = getRow()) {
 		Objekt = row.doc;
 
-		//Prüfen, ob Gruppen übergeben wurden
-		if (gruppen && gruppen.length > 0) {
-			//ja: Prüfen, ob das Dokument einer der Gruppen angehört / nein: weiter
-			if (Objekt.Gruppe.indexOf(gruppen) > -1) {
-				//diese Gruppe wollen wir
-				objektHinzufügen = true;
-			} else {
-				//Gruppen werden gefiltert und Filter ist nicht erfüllt > weiter mit nächstem Objekt
-				continue objekt_loop;
-			}
+		if (gruppen.indexOf(Objekt.Gruppe) === -1) {
+			//diese Gruppe wollen wir nicht > weiter mit nächstem Objekt
+			continue objekt_loop;
 		}
 
 		//row.key[1] ist 0, wenn es sich um ein Synonym handelt, dessen Informationen geholt werden sollen
 		if (row.key[1] === 0) {
-			if (Objekt.Beziehungssammlungen && Objekt.Beziehungssammlungen.length > 0) {
-				var ds_aus_syn_namen = [];
-				if (datensammlungen_aus_synonymen.length > 0) {
-					for (i=0; i<datensammlungen_aus_synonymen.length; i++) {
-						if (datensammlungen_aus_synonymen[i].Name) {
-							ds_aus_syn_namen.push(datensammlungen_aus_synonymen[i].Name);
+			if (Objekt.Datensammlungen && Objekt.Datensammlungen.length > 0) {
+					var ds_aus_syn_namen = [];
+					if (datensammlungen_aus_synonymen.length > 0) {
+						for (i=0; i<datensammlungen_aus_synonymen.length; i++) {
+							if (datensammlungen_aus_synonymen[i].Name) {
+								ds_aus_syn_namen.push(datensammlungen_aus_synonymen[i].Name);
+							}
+						}
+					}
+					var ds_aus_syn_name;
+					if (Objekt.Datensammlungen.length > 0) {
+						for (i=0; i<Objekt.Datensammlungen.length; i++) {
+							ds_aus_syn_name = Objekt.Datensammlungen[i].Name;
+							if (ds_aus_syn_namen.length === 0 || ds_aus_syn_name.indexOf(ds_aus_syn_namen) === -1) {
+								datensammlungen_aus_synonymen.push(Objekt.Datensammlungen[i]);
+								//sicherstellen, dass diese ds nicht nochmals gepuscht wird
+								ds_aus_syn_namen.push(ds_aus_syn_name);
+							}
 						}
 					}
 				}
-				var ds_aus_syn_name;
-				if (Objekt.Datensammlungen.length > 0) {
-					for (i=0; i<Objekt.Datensammlungen.length; i++) {
-						ds_aus_syn_name = Objekt.Datensammlungen[i].Name;
-						if (ds_aus_syn_namen.length === 0 || ds_aus_syn_name.indexOf(ds_aus_syn_namen) === -1) {
-							datensammlungen_aus_synonymen.push(Objekt.Datensammlungen[i]);
-							//sicherstellen, dass diese ds nicht nochmals gepuscht wird
-							ds_aus_syn_namen.push(ds_aus_syn_name);
+				if (Objekt.Beziehungssammlungen && Objekt.Beziehungssammlungen.length > 0) {
+					var bs_aus_syn_namen = [];
+					if (beziehungssammlungen_aus_synonymen.length > 0) {
+						for (i=0; i<beziehungssammlungen_aus_synonymen.length; i++) {
+							if (beziehungssammlungen_aus_synonymen[i].Name) {
+								bs_aus_syn_namen.push(beziehungssammlungen_aus_synonymen[i].Name);
+							}
+						}
+					}
+					var bs_aus_syn_name;
+					if (Objekt.Beziehungssammlungen.length > 0) {
+						for (i=0; i<Objekt.Beziehungssammlungen.length; i++) {
+							bs_aus_syn_name = Objekt.Beziehungssammlungen[i].Name;
+							if (bs_aus_syn_namen.length === 0 || bs_aus_syn_name.indexOf(bs_aus_syn_namen) === -1) {
+								beziehungssammlungen_aus_synonymen.push(Objekt.Beziehungssammlungen[i]);
+								//sicherstellen, dass diese bs nicht nochmals gepuscht wird
+								bs_aus_syn_namen.push(bs_aus_syn_name);
+							}
 						}
 					}
 				}
-
-				var bs_aus_syn_namen = [];
-				if (beziehungssammlungen_aus_synonymen.length > 0) {
-					for (i=0; i<beziehungssammlungen_aus_synonymen.length; i++) {
-						if (beziehungssammlungen_aus_synonymen[i].Name) {
-							bs_aus_syn_namen.push(beziehungssammlungen_aus_synonymen[i].Name);
-						}
-					}
-				}
-				var bs_aus_syn_name;
-				if (Objekt.Beziehungssammlungen.length > 0) {
-					for (i=0; i<Objekt.Beziehungssammlungen.length; i++) {
-						bs_aus_syn_name = Objekt.Beziehungssammlungen[i].Name;
-						if (bs_aus_syn_namen.length === 0 || bs_aus_syn_name.indexOf(bs_aus_syn_namen) === -1) {
-							beziehungssammlungen_aus_synonymen.push(Objekt.Beziehungssammlungen[i]);
-							//sicherstellen, dass diese bs nicht nochmals gepuscht wird
-							bs_aus_syn_namen.push(bs_aus_syn_name);
-						}
-					}
-				}
-			}
 			//das war ein Synonym. Hier aufhören
 		} else if (row.key[1] === 1) {
 			//wir sind jetzt im Originalobjekt
@@ -222,148 +133,75 @@ function(head, req) {
 
 			//exportobjekt gründen bzw. zurücksetzen
 			exportObjekt = {};
+			exportObjekt.Gruppe = Objekt.Gruppe;
+			exportObjekt.Ref = Objekt.Taxonomie.Daten["Taxonomie ID"];
 
-			//Neues Objekt aufbauen, das nur die gewünschten Felder enthält
-			for (var e in Objekt) {
-				//durch alle Eigenschaften des Dokuments loopen
-				if (typeof Objekt[e] !== "object" && e !== "_rev") {
-					for (i=0; i<felder.length; i++) {
-						if (felder[i].DsName === "Objekt" && felder[i].Feldname === e) {
-							if (felder[i].FeldnameOutput) {
-								//Standardfelder umbenennen
-								exportObjekt[felder[i].FeldnameOutput] = Objekt[e];
-							} else {
-								exportObjekt[e] = Objekt[e];
-							}
-						}
-						if (felder[i].DsName === "Objekt" && felder[i].Feldname === "GUID" && e === "_id") {
-							exportObjekt["GUID"] = Objekt[e];
-						}
-					}
-				}
+			var ds_zh_gis = _.find(Objekt.Datensammlungen, function(ds) {
+				return ds.Name === "ZH GIS";
+			}) || {};
+
+			exportObjekt.GISLayer = "";
+			if (ds_zh_gis && ds_zh_gis.Daten && ds_zh_gis.Daten["GIS-Layer"]) {
+				exportObjekt.GISLayer = ds_zh_gis.Daten["GIS-Layer"];
 			}
-			for (var w in felder) {
-				if (felder[w].DsTyp === "Taxonomie") {
-					//wenn im Objekt das zu exportierende Feld vorkommt, den Wert übernehmen
-					if (typeof Objekt.Taxonomie.Daten[felder[w].Feldname] !== "undefined") {
-						if (felder[w].FeldnameOutput) {
-							//Standardfelder umbenennen
-							exportObjekt[felder[w].FeldnameOutput] = Objekt.Taxonomie.Daten[felder[w].Feldname];
-						} else {
-							exportObjekt["Taxonomie(n): " + felder[w].Feldname] = Objekt.Taxonomie.Daten[felder[w].Feldname];
-						}
-					} else {
-						//sonst einen leerwert setzen
-						if (felder[w].FeldnameOutput) {
-							//Standardfelder umbenennen
-							exportObjekt[felder[w].FeldnameOutput] = "";
-						} else {
-							exportObjekt["Taxonomie(n): " + felder[w].Feldname] = "";
-						}
-					}
-				}
 
-				if (felder[w].DsTyp === "Datensammlung") {
-					//das leere feld setzen. Wird überschrieben, falls danach ein Wert gefunden wird
-					if (felder[w].FeldnameOutput) {
-						//Standardfelder umbenennen
-						exportObjekt[felder[w].FeldnameOutput] = "";
-					} else {
-						exportObjekt[felder[w].DsName + ": " + felder[w].Feldname] = "";
-					}
-					if (Objekt.Datensammlungen && Objekt.Datensammlungen.length > 0) {
-						//suchen, ob das Objekt diese Datensammlung hat
-						loop_ds:
-						for (var ii in Objekt.Datensammlungen) {
-							if (Objekt.Datensammlungen[ii].Name && Objekt.Datensammlungen[ii].Name === felder[w].DsName) {
-								if (typeof Objekt.Datensammlungen[ii].Daten[felder[w].Feldname] !== "undefined") {
-									if (felder[w].FeldnameOutput) {
-										//Standardfelder umbenennen
-										exportObjekt[felder[w].FeldnameOutput] = Objekt.Datensammlungen[ii].Daten[felder[w].Feldname];
-									} else {
-										exportObjekt[felder[w].DsName + ": " + felder[w].Feldname] = Objekt.Datensammlungen[ii].Daten[felder[w].Feldname];
-									}
-								}
-								break loop_ds;
-							}
-						}
-					}
-				}
-
-				if (felder[w].DsTyp === "Beziehung") {
-					//das leere feld setzen. Wird überschrieben, falls danach ein Wert gefunden wird
-					exportObjekt[felder[w].DsName + ": " + felder[w].Feldname] = "";
-					//wurde schon ein zusätzliches Feld geschaffen? wenn ja: hinzufügen
-
-					if (felder[w].Feldname === "Beziehungspartner") {
-						//noch ein Feld hinzufügen
-						exportObjekt[felder[w].DsName + ": Beziehungspartner GUID(s)"] = "";
-					}
-
-					if (Objekt.Beziehungssammlungen && Objekt.Beziehungssammlungen.length > 0) {
-						//suchen, ob das Objekt diese Beziehungssammlungen hat
-						loop_bs:
-						for (i in Objekt.Beziehungssammlungen) {
-							if (Objekt.Beziehungssammlungen[i].Name && Objekt.Beziehungssammlungen[i].Name === felder[w].DsName) {
-								//durch Beziehungen loopen
-								for (var aaa=0; aaa<Objekt.Beziehungssammlungen[i].Beziehungen.length; aaa++) {
-									if (typeof Objekt.Beziehungssammlungen[i].Beziehungen[aaa][felder[w].Feldname] !== "undefined") {
-										feldwert = convertToCorrectType(Objekt.Beziehungssammlungen[i].Beziehungen[aaa][felder[w].Feldname]);
-										//in der Beziehung gibt es das gesuchte Feld
-										//Beziehungen in der Variablen "exportBeziehungen" sammeln
-										//durch alle Beziehungen loopen und nur diejenigen anfügen, welche die Bedingungen erfüllen
-										var exportBeziehungen = [];
-										//Beziehung hinzufügen, aber sicherstellen, dass sie nicht schon drin ist
-										if (!containsObject(Objekt.Beziehungssammlungen[i].Beziehungen[aaa], exportBeziehungen)) {
-											exportBeziehungen.push(Objekt.Beziehungssammlungen[i].Beziehungen[aaa]);
-										}
-										//jeden Treffer kommagetrennt in dasselbe Feld einfügen
-										//durch Beziehungen loopen
-										for (var qq=0; qq<exportBeziehungen.length; qq++) {
-											//durch die Felder der Beziehung loopen
-											for (var yy in exportBeziehungen[qq]) {
-												if (yy === "Beziehungspartner") {
-													//zuerst die Beziehungspartner in JSON hinzufügen
-													if (!exportObjekt[felder[w].DsName + ": " + yy]) {
-														exportObjekt[felder[w].DsName + ": " + yy] = [];
-													}
-													exportObjekt[felder[w].DsName + ": " + yy].push(exportBeziehungen[qq][yy]);
-													//Reines GUID-Feld ergänzen
-													if (!exportObjekt[felder[w].DsName + ": Beziehungspartner GUID(s)"]) {
-														exportObjekt[felder[w].DsName + ": Beziehungspartner GUID(s)"] = exportBeziehungen[qq][yy][0].GUID;
-													} else {
-														exportObjekt[felder[w].DsName + ": Beziehungspartner GUID(s)"] += ", " + exportBeziehungen[qq][yy][0].GUID;
-													}
-												//es gibt einen Fehler, wenn replace für einen leeren Wert ausgeführt wird, also kontrollieren
-												} else if (typeof exportBeziehungen[qq][yy] === "number") {
-													//Vorsicht: in Nummern können keine Kommas ersetzt werden - gäbe einen error
-													if (!exportObjekt[felder[w].DsName + ": " + yy]) {
-														exportObjekt[felder[w].DsName + ": " + yy] = exportBeziehungen[qq][yy];
-													} else {
-														exportObjekt[felder[w].DsName + ": " + yy] += ", " + exportBeziehungen[qq][yy];
-													}
-												} else {
-													//Vorsicht: Werte werden kommagetrennt. Also müssen Kommas ersetzt werden
-													if (!exportObjekt[felder[w].DsName + ": " + yy]) {
-														exportObjekt[felder[w].DsName + ": " + yy] = exportBeziehungen[qq][yy].replace(/,/g,'\(Komma\)');
-													} else {
-														exportObjekt[felder[w].DsName + ": " + yy] += ", " + exportBeziehungen[qq][yy].replace(/,/g,'\(Komma\)');
-													}
-												}
-											}
-										}
-									}
-								}
-								break loop_bs;
-							}
-						}
-					}
-				}
+			exportObjekt.Distanz = "";
+			if (ds_zh_gis && ds_zh_gis.Daten && ds_zh_gis.Daten["Betrachtungsdistanz (m)"]) {
+				exportObjekt.Distanz = ds_zh_gis.Daten["Betrachtungsdistanz (m)"];
 			}
-			//Objekt zu Exportobjekten hinzufügen - wenn nicht schon kopiert
-			if (!schonKopiert) {
-				exportObjekte.push(exportObjekt);
+
+			exportObjekt.NameLat = Objekt.Taxonomie.Daten.Artname;
+			exportObjekt.NameDeu = "";
+			if (Objekt.Taxonomie.Daten["Name Deutsch"]) {
+				exportObjekt.NameDeu = Objekt.Taxonomie.Daten["Name Deutsch"];
 			}
+
+			var ds_zh_artwert_1995 = _.find(Objekt.Datensammlungen, function(ds) {
+				return ds.Name === "ZH Artwert (1995)";
+			}) || {};
+
+			exportObjekt.Artwert = "";
+			if (ds_zh_artwert_1995 && ds_zh_artwert_1995.Daten && (ds_zh_artwert_1995.Daten.Artwert || ds_zh_artwert_1995.Daten.Artwert === 0)) {
+				exportObjekt.Artwert = ds_zh_artwert_1995.Daten.Artwert;
+			}
+
+			exportObjekt.AwZusatz = "";
+			if (ds_zh_artwert_1995 && ds_zh_artwert_1995.Daten && ds_zh_artwert_1995.Daten["Artwert Zusatz"]) {
+				exportObjekt.AwZusatz = ds_zh_artwert_1995.Daten["Artwert Zusatz"];
+			}
+
+			var ds_blaue_liste = _.find(Objekt.Datensammlungen, function(ds) {
+				return ds.Name === "Blaue Liste (1998)";
+			}) || {};
+
+			exportObjekt.Bestandesentwicklung = "";
+			if (ds_blaue_liste && ds_blaue_liste.Daten && ds_blaue_liste.Daten.Bestandesentwicklung) {
+				exportObjekt.Bestandesentwicklung = ds_blaue_liste.Daten.Bestandesentwicklung;
+			}
+
+			exportObjekt.Schutzmassnahmen = "";
+			if (ds_blaue_liste && ds_blaue_liste.Daten && ds_blaue_liste.Daten.Schutzmassnahmen) {
+				exportObjekt.Schutzmassnahmen = ds_blaue_liste.Daten.Schutzmassnahmen;
+			}
+
+			exportObjekt.Wirksamkeit = "";
+			if (ds_blaue_liste && ds_blaue_liste.Daten && ds_blaue_liste.Daten.Wirksamkeit) {
+				exportObjekt.Wirksamkeit = ds_blaue_liste.Daten.Wirksamkeit;
+			}
+
+			var ds_zh_ap_flora = _.find(Objekt.Datensammlungen, function(ds) {
+				return ds.Name === "ZH AP Flora";
+			}) || {};
+
+			exportObjekt.Link_zum_AP_Bericht = "";
+			if (ds_zh_ap_flora && ds_zh_ap_flora.Daten && ds_zh_ap_flora.Daten["Link zum AP-Bericht"]) {
+				exportObjekt.Link_zum_AP_Bericht = ds_zh_ap_flora.Daten["Link zum AP-Bericht"];
+			}
+
+			exportObjekt["GUID_FNS"] = Objekt._id;
+			
+			//Objekt zu Exportobjekten hinzufügen
+			exportObjekte.push(exportObjekt);
 			//arrays für sammlungen aus synonymen zurücksetzen
 			beziehungssammlungen_aus_synonymen = [];
 			datensammlungen_aus_synonymen = [];
@@ -415,56 +253,4 @@ function erstelleExportString(exportobjekte) {
 		stringZeilen += stringZeile;
 	}
 	return stringTitelzeile + "\n" + stringZeilen;
-}
-
-function containsObject(obj, list) {
-	var i;
-	for (i = 0; i < list.length; i++) {
-		if (list[i] === obj) {
-			return true;
-		}
-	}
-	return false;
-}
-
-function convertToCorrectType(feldWert) {
-	var type = myTypeOf(feldWert);
-	if (type === "boolean") {
-		return Boolean(feldWert);
-	} else if (type === "float") {
-		return parseFloat(feldWert);
-	} else if (type === "integer") {
-		return parseInt(feldWert, 10);
-	} else if (type === "string") {
-		//string jetzt kleinschreiben, damit das nicht später erfolgen muss
-		return feldWert.toLowerCase();
-	} else {
-		//object nicht umwandeln. Man muss beim Vergleichen unterscheiden können, ob es ein Object war
-		return feldWert;
-	}
-}
-
-//Hilfsfunktion, die typeof ersetzt und ergänzt
-//typeof gibt bei input-Feldern immer String zurück!
-function myTypeOf(Wert) {
-	if (typeof Wert === "boolean") {
-		return "boolean";
-	} else if (parseInt(Wert, 10) && parseFloat(Wert) && parseInt(Wert, 10) !== parseFloat(Wert) && parseInt(Wert, 10) == Wert) {
-		//es ist eine Float
-		return "float";
-	//verhindern, dass führende Nullen abgeschnitten werden
-	} else if ((parseInt(Wert, 10) == Wert && Wert.toString().length === Math.ceil(parseInt(Wert, 10)/10)) || Wert == "0") {
-		//es ist eine Integer
-		return "integer";
-	} else if (typeof Wert === "object") {
-		//es ist ein Objekt
-		return "object";
-	} else if (typeof Wert === "string") {
-		//als String behandeln
-		return "string";
-	} else if (typeof Wert === "undefined") {
-		return "undefined";
-	} else if (typeof Wert === "function") {
-		return "function";
-	}
 }
