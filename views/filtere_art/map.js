23276ab4b1fc30fed1a583e375c0e4b5c357e818
Alexander Gabriel <alex.barbalex@gmail.com>
2014-02-19 15:34:55 +0100

    Filter korrigiert: Bringt auch Vorschläge, wenn kein deutscher Name

--- views/filtere_art/map.js
+++ views/filtere_art/map.js
@@ -1,7 +1,7 @@
 function(doc) {
 	var value = {};
 	var i;
-	var name_deutsch_worte;
+	var name_deutsch_worte, namen_deutsch_worte, name_worte;
 	if (doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Daten && doc.Taxonomie.Daten["Artname vollständig"]) {
 		if (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Macromycetes" || doc.Gruppe === "Moose") {
 			value.Name = doc.Taxonomie.Daten["Artname vollständig"];
@@ -11,28 +11,36 @@ function(doc) {
 				value.tokens.push(doc.Taxonomie.Daten.Gattung);
 				value.tokens.push(doc.Taxonomie.Daten.Art);
 				value.tokens.push(doc.Taxonomie.Daten.Autor);
-				name_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
-				for (i=0; i<name_deutsch_worte.length; i++) {
-					value.tokens.push(name_deutsch_worte[i]);
+				if (doc.Taxonomie.Daten["Name Deutsch"]) {
+					name_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
+					for (i=0; i<name_deutsch_worte.length; i++) {
+						value.tokens.push(name_deutsch_worte[i]);
+					}
 				}
 			} else if (doc.Gruppe === "Flora") {
 				value.tokens.push(doc.Taxonomie.Daten.Gattung);
 				value.tokens.push(doc.Taxonomie.Daten.Art);
 				value.tokens.push(doc.Taxonomie.Daten.Autor);
-				var namen_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
-				for (i=0; i<namen_deutsch_worte.length; i++) {
-					value.tokens.push(namen_deutsch_worte[i]);
+				if (doc.Taxonomie.Daten["Name Deutsch"]) {
+					namen_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
+					for (i=0; i<namen_deutsch_worte.length; i++) {
+						value.tokens.push(namen_deutsch_worte[i]);
+					}
 				}
 			} else if (doc.Gruppe === "Macromycetes") {
 				value.tokens.push(doc.Taxonomie.Daten.Gattung);
-				var name_worte = doc.Taxonomie.Daten.Name.split(" ");
-				for (i=0; i<name_worte.length; i++) {
-					value.tokens.push(name_worte[i]);
+				if (doc.Taxonomie.Daten.Name) {
+					name_worte = doc.Taxonomie.Daten.Name.split(" ");
+					for (i=0; i<name_worte.length; i++) {
+						value.tokens.push(name_worte[i]);
+					}
 				}
 				value.tokens.push(doc.Taxonomie.Daten.Autor);
-				name_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
-				for (i=0; i<name_deutsch_worte.length; i++) {
-					value.tokens.push(name_deutsch_worte[i]);
+				if (doc.Taxonomie.Daten["Name Deutsch"]) {
+					name_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
+					for (i=0; i<name_deutsch_worte.length; i++) {
+						value.tokens.push(name_deutsch_worte[i]);
+					}
 				}
 			} else if (doc.Gruppe === "Moose") {
 				value.tokens.push(doc.Taxonomie.Daten.Gattung);
