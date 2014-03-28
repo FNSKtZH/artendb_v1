be9c44d438b71a5d274cc4242a78b53cfb29211e
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-03-28 17:34:30 +0100

    Suche: Code verbessert. Keine Suche nach GUID und Taxonomie ID

--- views/filtere_art/map.js
+++ views/filtere_art/map.js
@@ -1,50 +1,33 @@
 function(doc) {
-	var value = {};
-	var i;
-	var name_deutsch_worte, namen_deutsch_worte, name_worte, autor_worte;
-	if (doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Daten && doc.Taxonomie.Daten["Artname vollständig"]) {
+	var value = {},
+		i,
+		artname_vollstaendig = doc.Taxonomie.Daten["Artname vollständig"],
+		artname_vollstaendig_worte;
+	if (doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Daten && artname_vollstaendig) {
 		if (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Macromycetes" || doc.Gruppe === "Moose") {
 
 			// value.Name: dieser Name wird als Suchresultat angezeigt
-			value.Name = doc.Taxonomie.Daten["Artname vollständig"];
+			value.Name = artname_vollstaendig;
 			// mit dieser id wird der Datensatz geholt
 			value.id = doc._id;
 			// nach den in tokens enthaltenen Begriffen wird gesucht
 			value.tokens = [];
 
+			// Artnamen vollständig auftrennen
+			artname_vollstaendig_worte = artname_vollstaendig.split(" ");
+			for (i=0; i<artname_vollstaendig_worte.length; i++) {
+				// Klammern entfernen. Sonst findet die Suche nach "Erd" keine Erdkröte (nur die Suche nach "(Erd")
+				value.tokens.push(artname_vollstaendig_worte[i].replace("\(", "", "g").replace("\)", "", "g"));
+			}
+
 			// Idee: GUID und Taxonomie Id als token ergänzen
-			value.dokens.push(doc._id);
+			// funktioniert nicht, daher ausgeschaltet
+			/*value.tokens.push(doc._id);
 			if (doc.Taxonomie.Daten["Taxonomie ID"]) {
 				value.tokens.push(doc.Taxonomie.Daten["Taxonomie ID"]);
-			}
-			// übrige Felder, die Teil des Namens sind
-			if (doc.Taxonomie.Daten.Gattung) {
-				value.tokens.push(doc.Taxonomie.Daten.Gattung);
-			}
-			if (doc.Taxonomie.Daten.Art) {
-				value.tokens.push(doc.Taxonomie.Daten.Art);
-			}
-			if (doc.Taxonomie.Daten.Autor) {
-				autor_worte = doc.Taxonomie.Daten.Autor.split(" ");
-				for (i=0; i<autor_worte.length; i++) {
-					value.tokens.push(autor_worte[i]);
-				}
-			}
-			if (doc.Taxonomie.Daten.Name) {
-				// das gibt es offenbar in macromycetes
-				name_worte = doc.Taxonomie.Daten.Name.split(" ");
-				for (i=0; i<name_worte.length; i++) {
-					value.tokens.push(name_worte[i]);
-				}
-			}
-			if (doc.Taxonomie.Daten["Name Deutsch"]) {
-				name_deutsch_worte = doc.Taxonomie.Daten["Name Deutsch"].split(" ");
-				for (i=0; i<name_deutsch_worte.length; i++) {
-					value.tokens.push(name_deutsch_worte[i]);
-				}
-			}
+			}*/
 			
-			emit ([doc.Gruppe, doc.Taxonomie.Daten["Artname vollständig"]], value);
+			emit ([doc.Gruppe, artname_vollstaendig], value);
 		}
 	}
 }
