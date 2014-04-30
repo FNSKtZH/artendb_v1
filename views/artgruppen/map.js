9327759c613f9c475afc0954094a29852325f0b9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-04-30 22:17:20 +0200

    Datenzugriff für evab mobile verbessert

--- views/artgruppen/map.js
+++ views/artgruppen/map.js
@@ -3,16 +3,18 @@
 	var i,
 		gis_layer;
 
-	if (doc.Typ && doc.Typ === "Objekt" && doc.Datensammlungen) {
-		ds_loop:
-		for (i=0; i<doc.Datensammlungen.length; i++) {
-			if (doc.Datensammlungen[i].Name === "ZH GIS" && doc.Datensammlungen[i].Daten && doc.Datensammlungen[i].Daten["GIS-Layer"]) {
-				gis_layer = doc.Datensammlungen[i].Daten["GIS-Layer"];
-				emit (gis_layer, null);
-				break ds_loop;
+	if (doc.Typ && doc.Typ === "Objekt") {
+		if (doc.Datensammlungen) {
+			ds_loop:
+			for (i=0; i<doc.Datensammlungen.length; i++) {
+				if (doc.Datensammlungen[i].Name && doc.Datensammlungen[i].Name === "ZH GIS" && doc.Datensammlungen[i].Daten && doc.Datensammlungen[i].Daten["GIS-Layer"]) {
+					gis_layer = doc.Datensammlungen[i].Daten["GIS-Layer"];
+					emit (gis_layer, null);
+					break ds_loop;
+				}
 			}
 		}
-		if (doc.Gruppe === "Macromycetes") {
+		if (doc.Gruppe === "Macromycetes" && !gis_layer) {
 			// momentan fehlen bei Macromycetes die ZH GIS
 			// diese Info wird für evab mobile benutzt
 			gis_layer = "Pilze";
--- views/evab_arten/map.js
+++ views/evab_arten/map.js
@@ -1,5 +1,25 @@
 ﻿function(doc) {
-	if (doc.Gruppe && (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Moose" || doc.Gruppe === "Macromycetes")) {
-		emit (doc._id);
+	var i,
+		gis_layer;
+
+	if (doc.Typ && doc.Typ === "Objekt" && doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Daten && doc.Taxonomie.Daten["Artname vollständig"] && (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Moose" || doc.Gruppe === "Macromycetes")) {
+
+		// gis-layer bestimmen
+		if (doc.Datensammlungen) {
+			ds_loop:
+			for (i=0; i<doc.Datensammlungen.length; i++) {
+				if (doc.Datensammlungen[i].Name && doc.Datensammlungen[i].Name === "ZH GIS" && doc.Datensammlungen[i].Daten && doc.Datensammlungen[i].Daten["GIS-Layer"]) {
+					gis_layer = doc.Datensammlungen[i].Daten["GIS-Layer"];
+					break ds_loop;
+				}
+			}
+		}
+		if (doc.Gruppe === "Macromycetes" && !gis_layer) {
+			// momentan fehlen bei Macromycetes die ZH GIS
+			// diese Info wird für evab mobile benutzt
+			gis_layer = "Pilze";
+		}
+
+		emit ([gis_layer, doc.Taxonomie.Daten["Artname vollständig"]], null);
 	}
 }
