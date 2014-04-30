2e0b4383c5132f2e9299b090fe4cd600afc65af9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-04-30 16:51:19 +0200

    Export für evab nach Artgruppe und Artname sortiert

--- views/evab_arten/map.js
+++ views/evab_arten/map.js
@@ -1,5 +1,5 @@
 ﻿function(doc) {
-	if (doc.Gruppe && (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Moose")) {
+	if (doc.Gruppe && (doc.Gruppe === "Fauna" || doc.Gruppe === "Flora" || doc.Gruppe === "Moose" || doc.Gruppe === "Macromycetes")) {
 		emit (doc._id);
 	}
 }
