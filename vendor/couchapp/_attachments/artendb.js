bdcec25404fa87e7b276a1dda378274b571a3bc3
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-05-26 20:49:30 +0200

    Direktexporte einer einzigen Gruppe verwenden den view der Gruppe

--- vendor/couchapp/_attachments/artendb.js
+++ vendor/couchapp/_attachments/artendb.js
@@ -4225,16 +4225,43 @@ window.adb.übergebeFilterFürExportFürAlt = function(gruppen, gruppen_array, a
 window.adb.übergebeFilterFürDirektExport = function(gruppen, gruppen_array, anz_ds_gewählt, filterkriterien_objekt, gewählte_felder_objekt) {
 	// Alle Felder abfragen
 	var fTz = "false",
-		queryParam;
+		queryParam,
+        view_name,
+        list_name,
+        gruppenliste = gruppen.split(",");
 	// window.adb.fasseTaxonomienZusammen steuert, ob Taxonomien alle einzeln oder unter dem Titel Taxonomien zusammengefasst werden
 	if (window.adb.fasseTaxonomienZusammen) {
 		fTz = "true";
 	}
 	if ($("#exportieren_synonym_infos").prop('checked')) {
-		queryParam = "export_mit_synonymen_direkt/all_docs_mit_synonymen?include_docs=true&filter=" + encodeURIComponent(JSON.stringify(filterkriterien_objekt)) + "&felder=" + encodeURIComponent(JSON.stringify(gewählte_felder_objekt)) + "&fasseTaxonomienZusammen=" + fTz + "&gruppen=" + gruppen;
+        list_name = "export_mit_synonymen_direkt";
+        if (gruppenliste.length > 1) {
+            view_name = "all_docs_mit_synonymen";
+        } else {
+            // den view der Gruppe nehmen, das ist viel schneller
+            if (gruppenliste[0] === "Lebensräume") {
+                view_name = "lr_mit_synonymen";
+            } else {
+                view_name = gruppenliste[0].toLowerCase() + "_mit_synonymen";
+            }
+        }
 	} else {
-		queryParam = "export_direkt/all_docs?include_docs=true&filter=" + encodeURIComponent(JSON.stringify(filterkriterien_objekt)) + "&felder=" + encodeURIComponent(JSON.stringify(gewählte_felder_objekt)) + "&fasseTaxonomienZusammen=" + fTz + "&gruppen=" + gruppen;
+        list_name = "export_direkt";
+        if (gruppenliste.length > 1) {
+            view_name = "all_docs";
+        } else {
+            // den view der Gruppe nehmen, das ist viel schneller
+            if (gruppenliste[0] === "Lebensräume") {
+                view_name = "lr";
+            } else {
+                view_name = gruppenliste[0].toLowerCase();
+            }
+
+        }
 	}
+
+    queryParam = list_name + "/" + view_name + "?include_docs=true&filter=" + encodeURIComponent(JSON.stringify(filterkriterien_objekt)) + "&felder=" + encodeURIComponent(JSON.stringify(gewählte_felder_objekt)) + "&fasseTaxonomienZusammen=" + fTz + "&gruppen=" + gruppen;
+
 	if ($("#exportieren_nur_objekte_mit_eigenschaften").prop('checked') && anz_ds_gewählt > 0) {
 		// prüfen, ob mindestens ein Feld aus ds gewählt ist
 		// wenn ja: true, sonst false
