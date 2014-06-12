6d2a46debb541148ea7cef1315665fdae1111e2d
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-06-12 15:45:19 +0200

    DS-Struktur umbauen: Funktion baut Struktur um

--- vendor/couchapp/_attachments/artendb.js
+++ vendor/couchapp/_attachments/artendb.js
@@ -1893,6 +1893,65 @@ window.adb.nenneDsUm = function() {
     });
 };
 
+window.adb.baueDsZuEigenschaftenUm = function() {
+    'use strict';
+    var $admin_baue_ds_zu_eigenschaften_um_rückmeldung = $("#admin_baue_ds_zu_eigenschaften_um_rückmeldung"),
+        $db = $.couch.db("artendb");
+    $admin_baue_ds_zu_eigenschaften_um_rückmeldung.html("Daten werden analysiert...");
+    $db.view('artendb/all_docs?&include_docs=true', {
+        success: function(data) {
+            var korrigiert = 0,
+                fehler = 0,
+                save;
+            if (data.rows.length === 0) {
+                $admin_baue_ds_zu_eigenschaften_um_rückmeldung.html("Keine Daten erhalten");
+                return;
+            }
+            _.each(data.rows, function(row) {
+                var art = row.doc,
+                    datensammlungen,
+                    beziehungssammlungen,
+                    ds_daten;
+                // Datensammlungen umbenennen
+                // ds und bs entfernen, danach in der richtigen Reihenfolge hinzufügen
+                // damit die Reihenfolge bewahrt bleibt
+                if (art.Datensammlungen) {
+                    datensammlungen = art.Datensammlungen;
+                    _.each(datensammlungen, function(ds) {
+                        if (ds.Daten) {
+                            ds_daten = ds.Daten;
+                            delete ds.Daten;
+                            ds.Eigenschaften = ds_daten;
+                        }
+                    });
+                    delete art.Datensammlungen;
+                    if (art.Beziehungssammlungen) {
+                        beziehungssammlungen = art.Beziehungssammlungen;
+                        delete art.Beziehungssammlungen;
+                    } else {
+                        beziehungssammlungen = {};
+                    }
+                    art.Eigenschaftensammlungen = datensammlungen;
+                    art.Beziehungssammlungen = beziehungssammlungen;
+                    $db.saveDoc(art, {
+                        success: function() {
+                            korrigiert ++;
+                            $admin_baue_ds_zu_eigenschaften_um_rückmeldung.html("Anzahl Dokumente in DB: " + data.rows.length + ". Umbenannt: " + korrigiert + ", Fehler: " + fehler);
+                        },
+                        error: function() {
+                            fehler ++;
+                            $admin_baue_ds_zu_eigenschaften_um_rückmeldung.html("Anzahl Dokumente in DB: " + data.rows.length + ". Umbenannt: " + korrigiert + ", Fehler: " + fehler);
+                        }
+                    });
+                }
+            });
+            if (korrigiert === 0) {
+                $("#admin_korrigiere_artwertname_in_flora_rückmeldung").html("Es gibt offenbar keine Datensammlungen mehr, die umbenannt werden müssen");
+            }
+        }
+    });
+};
+
 // wenn importieren_ds_ds_beschreiben_collapse geöffnet wird
 window.adb.handleImportierenDsDsBeschreibenCollapseShown = function() {
 	'use strict';
