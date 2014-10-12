0a4a8d2414af1d310ae4a970e504a4653cd9b095
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-10-12 21:59:50 +0200

    handleBsWählenChange modularisiert

---
+++ vendor/couchapp/_attachments/modules/handleBsWaehlenChange.js
@@ -0,0 +1,89 @@
+// Wenn BsWählen geändert wird
+
+'use strict';
+
+var _ = require('underscore');
+
+var returnFunction = function ($, that) {
+    var bs_name        = that.value,
+        wählbar        = false,
+        $BsAnzDs       = $("#BsAnzDs"),
+        $BsAnzDs_label = $("#BsAnzDs_label"),
+        $BsName        = $("#BsName"),
+        $importieren_bs_ds_beschreiben_hinweis2 = $("#importieren_bs_ds_beschreiben_hinweis2");
+    // allfälligen Alert schliessen
+    $importieren_bs_ds_beschreiben_hinweis2
+        .alert()
+        .hide();
+    // wählbar setzen
+    // wählen kann man nur, was man selber importiert hat - oder admin ist
+    if ($("#"+that.id+" option:selected").attr("waehlbar") === "true") {
+        wählbar = true;
+    } else if (Boolean(localStorage.admin)) {
+        wählbar = true;
+    }
+    if (wählbar) {
+        // zuerst alle Felder leeren
+        $('#importieren_bs_ds_beschreiben_collapse textarea, #importieren_bs_ds_beschreiben_collapse input').each(function() {
+            $(this).val('');
+        });
+        $BsAnzDs.html("");
+        $BsAnzDs_label.html("");
+        if (bs_name) {
+            _.each(window.adb.bs_von_objekten.rows, function(bs_row) {
+                if (bs_row.key[1] === bs_name) {
+                    $BsName.val(bs_name);
+                    _.each(bs_row, function(feldwert, feldname) {
+                        if (feldname === "Ursprungsdatensammlung") {
+                            $("#BsUrsprungsBs").val(feldwert);
+                        } else if (feldname !== "importiert von") {
+                            $("#Bs" + feldname).val(feldwert);
+                        }
+                    });
+                    if (bs_row.key[2] === true) {
+                        $("#BsZusammenfassend").prop('checked', true);
+                        $("#BsUrsprungsBs_div").show();
+                    } else {
+                        // sicherstellen, dass der Haken im Feld entfernt wird, wenn nach der zusammenfassenden eine andere BS gewählt wird
+                        $("#BsZusammenfassend").prop('checked', false);
+                        $("#BsUrsprungsBs_div").hide();
+                    }
+                    // wenn die ds/bs kein "importiert von" hat ist der Wert null
+                    // verhindern, dass null angezeigt wird
+                    if (bs_row.key[3]) {
+                        $("#BsImportiertVon").val(bs_row.key[3]);
+                    } else {
+                        $("#BsImportiertVon").val("");
+                    }
+                    $BsAnzDs_label.html("Anzahl Arten/Lebensräume");
+                    $BsAnzDs.html(bs_row.value);
+                    // dafür sorgen, dass textareas genug gross sind
+                    $('#importieren_bs').find('textarea').each(function() {
+                        window.adb.fitTextareaToContent(this, document.documentElement.clientHeight);
+                    });
+                    $BsName.focus();
+                }
+                // löschen-Schaltfläche einblenden
+                $("#BsLoeschen").show();
+            });
+        } else {
+            // löschen-Schaltfläche ausblenden
+            $("#BsLoeschen").hide();
+        }
+    } else {
+        // melden, dass diese BS nicht bearbeitet werden kann
+        $("#importieren_bs_ds_beschreiben_hinweis2_text")
+            .html("Sie können nur Beziehungssammlungen verändern, die Sie selber importiert haben.<br>Ausnahme: Zusammenfassende Beziehungssammlungen.");
+        $importieren_bs_ds_beschreiben_hinweis2
+            .alert()
+            .removeClass("alert-success")
+            .removeClass("alert-info")
+            .addClass("alert-danger")
+            .show();
+        $('html, body').animate({
+            scrollTop: $("#BsWaehlen").offset().top
+        }, 2000);
+    }
+};
+
+module.exports = returnFunction;
