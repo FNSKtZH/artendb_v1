9cee8cd0c0e85e23e9334b162a3ccc6b5120d3ff
Alexander Gabriel <alex.barbalex@gmail.com>
2014-10-17 21:14:04 +0200

    jquery.jstree reduziert

---
+++ gulp-tasks/prod_build_jstree.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
+
+gulp.task('prod_build_jstree', function() {
+    return gulp.src([
+	    	'vendor/couchapp/_attachments/jquery.jstree.js'
+    	])
+        .pipe(concat('jquery.jstree.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('vendor/couchapp/_attachments'))
+        .pipe(notify({ message: 'prod_build_jstree task beendet' }));
+});
