ba60483249e66e749e8991909623bf49797bf32d
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-11-14 16:57:40 +0100

    Code optimiert

--- lists/lib/beurteileObInformationenEnthaltenSind.js
+++ lists/lib/beurteileObInformationenEnthaltenSind.js
@@ -1,13 +1,12 @@
+// der Benutzer will nur Objekte mit Informationen aus den gewählten Eigenschaften- und Beziehungssammlungen erhalten
+// also müssen wir durch die Felder loopen und schauen, ob der Datensatz anzuzeigende Felder enthält
+
 /*jslint node: true, browser: true, nomen: true, todo: true, plusplus: true*/
 'use strict';
 
 var _ = require("lists/lib/underscore");
 
-module.exports = function (objekt, felder, filterkriterien) {
-    // der Benutzer will nur Objekte mit Informationen aus den gewählten Eigenschaften- und Beziehungssammlungen erhalten
-    // also müssen wir durch die Felder loopen und schauen, ob der Datensatz anzuzeigende Felder enthält
-    // wenn ja und Feld aus DS/BS und kein Filter gesetzt: objektHinzufügen = true
-    // wenn ein Filter gesetzt wurde und keine Daten enthalten sind, nicht anzeigen
+module.exports = function (objekt, felder) {
     var hinzufuegen = false,
         // es reicht, wenn mindestens ein feld mit Werten enthalten ist!
         mindestensEinFeldHinzufuegen = false,
@@ -15,7 +14,7 @@ module.exports = function (objekt, felder, filterkriterien) {
         feldName,
         bsMitName,
         bezMitFeldname,
-        ds_mit_name,
+        dsMitName,
         dsName;
 
     if (felder && felder.length > 0) {
@@ -41,11 +40,11 @@ module.exports = function (objekt, felder, filterkriterien) {
             } else if (dsTyp === "Datensammlung") {
                 // das ist ein Feld aus einer Datensammlung
                 // suche Datensammlung mit Name = dsName
-                ds_mit_name = _.find(objekt.Eigenschaftensammlungen, function (datensammlung) {
+                dsMitName = _.find(objekt.Eigenschaftensammlungen, function (datensammlung) {
                     return datensammlung.Name === dsName;
                 });
                 // hinzufuegen, wenn Feld mit feldName existiert und es Daten enthält
-                hinzufuegen = (ds_mit_name && ds_mit_name.Eigenschaften !== undefined && ds_mit_name.Eigenschaften[feldName] !== undefined);
+                hinzufuegen = (dsMitName && dsMitName.Eigenschaften !== undefined && dsMitName.Eigenschaften[feldName] !== undefined);
                 if (hinzufuegen) {
                     mindestensEinFeldHinzufuegen = true;
                 }
