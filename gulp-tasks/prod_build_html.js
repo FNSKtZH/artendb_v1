1db22e3c4903e062a7c5ba088e704e10ce042768
Alexander Gabriel <alex@gabriel-software.ch>
2015-03-23 10:51:13 +0100

    stopped minifying because of error

--- gulp-tasks/prod_build_html.js
+++ gulp-tasks/prod_build_html.js
@@ -9,7 +9,7 @@ var gulp       = require('gulp'),
 gulp.task('prod_build_html', function () {
     return gulp.src('_attachments/index_dev.html')
         // throws an error. Why?
-        .pipe(minifyHTML())
+        //.pipe(minifyHTML())
         .pipe(rename('index.html'))
         .pipe(gulp.dest('_attachments'))
         .pipe(notify({ message: 'prod_build_html task beendet' }));
