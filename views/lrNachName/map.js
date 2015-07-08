3211dde9dd7edf1aebfa13f097197533cb64edab
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-07-08 21:27:05 +0200

    lrNachName lists also top level elements

--- views/lrNachName/map.js
+++ views/lrNachName/map.js
@@ -1,7 +1,8 @@
 function (doc) {
     'use strict'
 
-    if (doc.Typ && doc.Typ === 'Objekt' && doc.Gruppe && doc.Gruppe === 'Lebensräume' && doc.Taxonomie && doc.Taxonomie.Name && doc.Taxonomie.Eigenschaften && doc.Taxonomie.Eigenschaften.Label && doc.Taxonomie.Eigenschaften.Einheit) {
+    if (doc.Typ && doc.Typ === 'Objekt' && doc.Gruppe && doc.Gruppe === 'Lebensräume' && doc.Taxonomie && doc.Taxonomie.Name && doc.Taxonomie.Eigenschaften && doc.Taxonomie.Eigenschaften.Einheit) {
+      var label = doc.Taxonomie.Eigenschaften.Label || null
         emit([doc.Taxonomie.Name, doc.Taxonomie.Eigenschaften.Label, doc.Taxonomie.Eigenschaften.Einheit])
     }
 }
