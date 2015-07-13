ba3ed1a31d2005798cee08610cbea75e69172d51
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-07-13 10:58:31 +0200

    view alt_arten prüft, ob Taxonomie ID existiert

--- views/alt_arten/map.js
+++ views/alt_arten/map.js
@@ -7,17 +7,20 @@ function (doc) {
 
   if (doc.Typ && doc.Typ === 'Objekt' && doc.Gruppe) {
     if (doc.Gruppe === 'Fauna' || doc.Gruppe === 'Flora') {
-      // sicherstellen, dass GIS-Layer und Betrachtungsdistanz existieren
-      if (doc.Eigenschaftensammlungen && doc.Eigenschaftensammlungen.length > 0) {
-        // durch alle Eigenschaftensammlungen loopen
-        _.each(doc.Eigenschaftensammlungen, function (datensammlung) {
-          if (datensammlung.Name && datensammlung.Name === 'ZH GIS' && datensammlung.Eigenschaften && datensammlung.Eigenschaften['GIS-Layer'] && datensammlung.Eigenschaften['Betrachtungsdistanz (m)']) {
-            // ok, alle benötigten Felder sind vorhanden
-            // erst mal das eigene Dokument senden
-            // der zweite key markiert, dass dies das Original ist
-            emit(doc._id)
-          }
-        })
+      // sicherstellen, dass Taxonomie-ID existiert
+      if (doc.Taxonomie && doc.Taxonomie.Eigenschaften && doc.Taxonomie.Eigenschaften['Taxonomie ID']) {
+        // sicherstellen, dass GIS-Layer und Betrachtungsdistanz existieren
+        if (doc.Eigenschaftensammlungen && doc.Eigenschaftensammlungen.length > 0) {
+          // durch alle Eigenschaftensammlungen loopen
+          _.each(doc.Eigenschaftensammlungen, function (datensammlung) {
+            if (datensammlung.Name && datensammlung.Name === 'ZH GIS' && datensammlung.Eigenschaften && datensammlung.Eigenschaften['GIS-Layer'] && datensammlung.Eigenschaften['Betrachtungsdistanz (m)']) {
+              // ok, alle benötigten Felder sind vorhanden
+              // erst mal das eigene Dokument senden
+              // der zweite key markiert, dass dies das Original ist
+              emit(doc._id)
+            }
+          })
+        }
       }
     }
   }
