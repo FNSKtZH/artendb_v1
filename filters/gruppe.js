c33621fbdd40853714b3aec464811caa0b556253
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-07-20 11:23:53 +0200

    corrected gruppe filter

--- filters/gruppe.js
+++ filters/gruppe.js
@@ -2,6 +2,8 @@ function (doc, req) {
     'use strict'
     // Zweck: DB nach Gruppe filtern
     var gruppe = req.query.gruppe
+    // need to pass Lebensräume as lr, otherwise curl on windows errors
+    if (gruppe === 'lr') gruppe = 'Lebensräume'
     if (doc.Gruppe && gruppe && doc.Gruppe === gruppe) return true
     return false
 }
